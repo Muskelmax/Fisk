96b10556b80122a2cd561b50e29c957ffda88453
Muskelmax <maxbuhler50@gmail.com>
2024-09-02 14:00:19 +0200

    test commit

---
+++ fluck.py
@@ -0,0 +1,23 @@
+from fish import *
+import random
+
+class Flock:
+  def __init__(self, amount):
+    self.__amount = amount
+    self.__fishies = []
+    for i in range(0, self.__amount + 1):
+      self.__fishies.append(Fish(random.random()*600, random.random()*500, random.random()*5,random.random()*5))
+  def draw(self, screen):
+    for fish in self.__fishies:
+      fish.render(screen)
+  def update(self):
+    for fish in self.__fishies:
+      fish.update()
+      fish.bordercheck()
+  def react(self, leftClick, rightClick):
+    for fish in self.__fishies:
+      if(leftClick == True):
+        fish.attract()
+      if(rightClick == True):
+        fish.avoid()
+
